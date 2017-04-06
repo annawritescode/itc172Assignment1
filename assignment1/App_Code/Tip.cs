29e20b4bee302a3c9c62b95d0853f557eedc1d80
annawritescode <anna.y.wong01@gmail.com>
2017-04-06 12:19:06 -0700

    Add files via upload
    Tip calculator

---
+++ assignment1/App_Code/Tip.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Tip
+/// </summary>
+public class Tip
+{   
+    //variables
+    private double mealAmount;
+    private double tipPercent;
+    //constant
+    private const double TAXPERCENT = .101;
+    public Tip(double amount, double tipPerc)
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+        mealAmount = amount;
+        tipPercent = tipPerc;
+    }
+
+    public double CalculateTax()
+    {
+        return mealAmount * TAXPERCENT;
+    }
+
+    public double CalculateTip()
+    {
+        return mealAmount * tipPercent;
+            }
+
+    public double Total()
+
+    {
+        return mealAmount + CalculateTax() + CalculateTip();
+
+
+    }
+
+}
---
+++ assignment1/Default.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            string[] percents = { "10%", "15%", "20%", "other" };
+            TipRadioButtonList.DataSource = percents;
+            TipRadioButtonList.DataBind();
+        }
+        
+    }
+
+    protected void SubmitButton_Click(object sender, EventArgs e)
+    {
+        GetTotals();
+    }
+
+    private void GetTotals()
+    {
+        //pass value
+        double amount;
+        Tip tip=null;
+        bool goodAmount= double.TryParse(MealTextBox.Text, out amount);
+        if(goodAmount)
+        {
+            //declare class
+            double percent = 0;
+            if(TipRadioButtonList.SelectedItem.Text != "other")
+            {
+                if (TipRadioButtonList.SelectedItem.Text.Equals("10%"))
+                    percent = .1;
+
+                if (TipRadioButtonList.SelectedItem.Text.Equals("15%"))
+                    percent = .15;
+
+                if (TipRadioButtonList.SelectedItem.Text.Equals("20%"))
+                    percent = .2;
+
+
+            }
+            else
+            {
+                percent = double.Parse(OtherTextBox.Text);
+                percent /= 100;
+            }
+             tip = new Tip(amount, percent);
+
+
+        }
+        else
+        {
+            Response.Write("<script>alert('Enter a valid amount');</script>");
+        }
+
+        //display result
+        ResultLabel.Text = "Amount:" + amount.ToString("$ #, ##0.00") + "<br/>" + "Tax:" 
+            + tip.CalculateTax().ToString("$ #, ##0.00") + "<br/>"
+            + "tip" + tip.CalculateTip().ToString("$ #, ##0.00") + "<br/>"
+            + "Total:" + tip.Total().ToString("$ #, ##0.00");
+
+
+    }
+}
