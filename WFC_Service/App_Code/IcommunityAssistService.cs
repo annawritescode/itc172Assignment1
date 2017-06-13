14b3d99a767b2a845feeafdd1063bbc8dc436a80
Anna Wong <anna.y.wong01@gmail.com>
2017-06-13 10:28:57 -0700

    Add files via upload
    WCF service assignment #5

---
+++ WFC_Service/App_Code/IcommunityAssistService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IcommunityAssistService" in both code and config file together.
+[ServiceContract]
+public interface IcommunityAssistService
+{
+    [OperationContract]
+    int Login(string user, string password);
+
+    [OperationContract]
+    bool RegisterPerson(PersonLite p);
+
+    [OperationContract]
+    List<GrantRequest> GetGrantRequest(int PersonKey);
+
+    [OperationContract]
+
+    List<GrantReview> GetGrantReviews(int GrantRequestKey);
+}
+
+
+[DataContract]
+
+public class PersonLite
+{
+    //add stored procedures for usp register
+    [DataMember]
+    public string lastname { get; set; }
+    [DataMember]
+    public string firstname { get; set; }
+    [DataMember]
+    public string email { get; set; }
+    [DataMember]
+    public string password { get; set; }
+
+    [DataMember]
+    public string ApartmentNumber { get; set; }
+    [DataMember]
+    public string street { get; set; }
+    [DataMember]
+    public string city { get; set; }
+    [DataMember]
+    public string state { get; set; }
+    [DataMember]
+    public string zipcode { get; set; }
+    [DataMember]
+    public string homePhone { get; set; }
+    [DataMember]
+    public string workPhone { get; set; }
+}
+    
---
+++ WFC_Service/App_Code/communityAssistService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "communityAssistService" in code, svc and config file together.
+public class communityAssistService : IcommunityAssistService
+{
+    Community_AssistEntities db = new Community_AssistEntities();
+
+    public List<GrantRequest> GetGrantRequest(int PersonKey)
+    {
+        var grant = from g in db.GrantRequests
+                    select g;
+                     
+        List<GrantRequest> grants = new List<GrantRequest>();
+        foreach (GrantRequest gr in grant )
+        {
+            GrantRequest grant1 = new GrantRequest();
+            grant1.GrantRequestDate = gr.GrantRequestDate;
+            grant1.PersonKey = gr.PersonKey;
+            grant1.GrantTypeKey = gr.GrantTypeKey;
+            grant1.GrantRequestExplanation = gr.GrantRequestExplanation;
+            grant1.GrantRequestAmount = gr.GrantRequestAmount;
+            
+            
+               
+            
+            grants.Add(grant1);
+        }
+        return grants;
+    }
+
+
+
+
+
+    public List<GrantRequest> GetGrantReview(int personKey)
+    {
+        var grnts = from g in db.GrantRequests
+                    from p in g.GrantReviews
+                    where g.PersonKey == personKey
+                    select g;
+    
+        List<GrantRequest> grantRequests = new List<GrantRequest>();
+
+        foreach (var gt in grnts)
+        {
+            GrantRequest gr = new GrantRequest();
+            gr.GrantRequestDate = gt.GrantRequestDate;
+            gr.GrantTypeKey = gt.GrantTypeKey;
+            gr.GrantRequestExplanation = gt.GrantRequestExplanation;
+            gr.GrantRequestAmount = gt.GrantRequestAmount;
+            gr.PersonKey = gt.PersonKey;
+        }
+        return grantRequests;
+    }
+
+    public List<GrantReview> GetGrantReviews(int GrantRequestKey)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Login(string user, string password)
+    {
+        int key = 0;
+        int  result = db.usp_Login(user, password);
+        if (result != -1)
+        {
+            var userKey = (from k in db.People
+                           where k.PersonKey.Equals(user)
+                           select k.PersonKey).FirstOrDefault();
+            key = (int)userKey;
+        }
+        return key;
+
+    }
+
+    public bool RegisterPerson(PersonLite p)
+    {
+        bool result = true;
+        int per = db.usp_Register(p.lastname,
+            p.firstname, p.email, p.password,
+            p.ApartmentNumber, p.street, p.city, p.state, p.zipcode, p.homePhone, p.workPhone);
+
+            return result;
+    }
+}
